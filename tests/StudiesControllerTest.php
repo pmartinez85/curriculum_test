280602ed46acf2ad08c4325ab6711ea50464cfc1
Pedro Martinez <pmartinez1085@gmail.com>
2016-12-13 17:52:22 +0100

    3 tests not ok

--- tests/StudiesControllerTest.php
+++ tests/StudiesControllerTest.php
@@ -1,14 +1,19 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+
 
 /**
  * Class StudiesControllerTest
+ * @property  response
  */
+
 class StudiesControllerTest extends TestCase
 {
+
+    use Illuminate\Foundation\Testing\WithoutMiddleware;
+    use Illuminate\Foundation\Testing\DatabaseMigrations;
+    use Illuminate\Foundation\Testing\DatabaseTransactions;
+
     /**
      * A basic test example.
      *
@@ -16,10 +21,43 @@ class StudiesControllerTest extends TestCase
      */
     public function testIndex()
     {
-        //dd(route('studies.index'));
-       $this->call('GET','studies.index');
+//        $studies1 = factory(\Scool\Curriculum\Models\Study::class,50)->make();
+        $this->login();
+        $this->get('studies');
+        $this->assertResponseOk();
+        $this->assertViewHas('studies');
+
+        //$studies = $this->response->getOriginalContent()->getdata()['studies'];
+
+       // $this->assertInstanceOf(Illuminate\Database\Eloquent\Collection::class, $studies);
+
+//        dd($studies);
+        //dd($this->call('GET','studies'));  //val esta o la de baix amb el dump
+        //$this->get('studies')->dump();
         //1) Preparacio
         //2) Execucio
         //3) Assertions
+    }
+
+    public function testIndexNotLogged()
+    {
+        $this->get('studies');
+        $this->assertRedirectedTo('login');
+
+    }
+
+    public function testStore()
+    {
+        $this->login();
+        $this->post('studies');
+        //$this->post('studies')->dump('post');
+        $this->assertRedirectedToRoute('studies.create');
+
+    }
+
+    public function login()
+    {
+        $user = factory(App\User::class)->create();
+        $this->actingAs($user);
     }
 }
