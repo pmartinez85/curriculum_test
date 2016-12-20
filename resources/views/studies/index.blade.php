872478cb30ad038ac9606f9369de88d5e2a997c9
Pedro Martinez <pmartinez1085@gmail.com>
2016-12-20 17:20:53 +0100

    add forms and errors

--- resources/views/studies/index.blade.php
+++ resources/views/studies/index.blade.php
@@ -10,8 +10,8 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
 
-                <div class="box box-primary"></div>
-                <div class="box box-default collapsed-box">
+
+                <div class="box box-default">
                     <div class="box-header with-border">
                         <h3 class="box-title">Create Study</h3>
 
@@ -22,14 +22,56 @@
                             </button>
                         </div>
                         <!-- /.box-tools -->
-                    </div>
-                    <!-- /.box-header -->
-                    <div class="box-body">
-                        The body of the box
-                    </div>
-                    <!-- /.box-body -->
-                </div>
 
+                    </div>
+
+
+                    <div class="box-body">
+                        <div class="form-group has-error">
+                        <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i> Input with
+                        error</label>
+                        <input type="text" class="form-control" id="inputError" placeholder="Enter ...">
+                        <span class="help-block">Help block with error</span>
+                        </div>
+
+
+
+
+                        <div class="form-group has-warning">
+                        <label class="control-label" for="inputWarning"><i class="fa fa-bell-o"></i> Input with
+                        warning</label>
+                        <input type="text" class="form-control" id="inputWarning" placeholder="Enter ...">
+                        <span class="help-block">Help block with warning</span>
+                        </div>
+
+                    <form role="form" action="/studies" method="post">
+
+                       {{ csrf_field() }}
+
+                    <input type="hidden" name="law_id" value="1">
+                        @if ($errors->has('name'))
+                        <div class="form-group has-warning">
+                            <label class="control-label" for="inputWarning"><i class="fa fa-check"></i> Name with warning </label>
+                            <input type="text" class="form-control" id="inputWarning" placeholder="Name here..." name="name">
+                            @foreach ($errors->get('name') as $message)
+                                <span class="help-block">{{$message}}</span>
+                            @endforeach
+                        </div>
+                        @else
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" class="form-control" placeholder="Name" name="name" id="name">
+                            </div>
+                        @endif
+                        <input type="submit" name="create">
+                    </form>
+                    </div>
+
+                    <!-- /.box-body -->
+                    <div class="box-footer">
+                        <button class="btn btn-primary" type="submit">Create</button>
+                    </div>
+                </div>
 
 
                 <div class="box box-default">
